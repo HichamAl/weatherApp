f5be693c5e820a8e85cd681ebef71b4c3a0e8865
HichamAl <h.almakroudi@icloud.com>
2025-12-03 20:36:02 +0100

    create async function that takes location and fetches data from api, and calls newly created factory function which gets only necessary info for the location weather and makes an object containing that data

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("test");
+const WeatherLocation = "Nador";
+
+async function getWeatherData(WeatherLocation){
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${WeatherLocation}?unitGroup=metric&include=days%2Ccurrent&key=YY5KXEMSNCSTRC4K7AD94FQ97&contentType=json`);
+    const jsonResponse = await response.json();
+    const weatherData = weatherObject(jsonResponse);
+    console.log(weatherData);
+}
+
+function weatherObject(jsonResponse){
+    let day = jsonResponse.days[0].datetime;
+    let conditions = jsonResponse.days[0].conditions;
+    let feelslike = jsonResponse.days[0].feelslike;
+    let humidity = jsonResponse.days[0].humidity;
+    let temp = jsonResponse.days[0].temp;
+    return { day, conditions, feelslike, humidity, temp }
+}
+
+getWeatherData(WeatherLocation);
