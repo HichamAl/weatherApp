9e6b63a8d1292f29cbd7a647742497012b80ce8d
HichamAl <h.almakroudi@icloud.com>
2025-12-03 21:35:25 +0100

    add code that gets data from object and displays it on the page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("#form");
+const resultsDiv = document.querySelector(".results");
 form.addEventListener("submit", function(event){
     event.preventDefault();
     const searchQuery = document.querySelector("#searchbox").value;
@@ -10,18 +11,20 @@ async function getWeatherData(WeatherLocation){
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${WeatherLocation}?unitGroup=metric&include=days%2Ccurrent&key=YY5KXEMSNCSTRC4K7AD94FQ97&contentType=json`);
         const jsonResponse = await response.json();
         const weatherData = weatherObject(jsonResponse);
-        console.log(weatherData);
+        const { address,day,conditions,feelslike,humidity,temp} = weatherData;
+        resultsDiv.innerHTML = `Location: ${address} <br>Date: ${day}<br>Condition: ${conditions} <br>Feelslike temperature: ${feelslike}<br>humidity: ${humidity}<br>Temperature: ${temp}`;
     } catch(error) {
         console.log(error);
     }  
 }
 
 function weatherObject(jsonResponse){
+    console.log(jsonResponse);
+    let address = jsonResponse.address;
     let day = jsonResponse.days[0].datetime;
     let conditions = jsonResponse.days[0].conditions;
     let feelslike = jsonResponse.days[0].feelslike;
     let humidity = jsonResponse.days[0].humidity;
     let temp = jsonResponse.days[0].temp;
-    return { day, conditions, feelslike, humidity, temp }
+    return { address, day, conditions, feelslike, humidity, temp }
 }
-
